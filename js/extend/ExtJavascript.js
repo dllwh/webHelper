db139dbf2a5bb1b88197b32fd75d036d2fefebbd
皇族灬战狼 <duleilewuhen@163.com>
2017-12-14 20:44:46 +0800

    扩展

--- js/extend/ExtJavascript.js
+++ js/extend/ExtJavascript.js
@@ -32,3 +32,125 @@ $(".chackTextarea-area").autoTextarea({
     maxHeight:400,
     minHeight:100
 })
+
+/**
+ * ==================================================================
+ * 
+ * 鼠标经过函数
+ * 
+ * @param options
+ *            鼠标经过离开时样式
+ * @param enterCallback
+ *            鼠标经过回调
+ * @param leaveCallback
+ *            鼠标离开回调
+ * 
+ * ===================================================================
+ */
+!function($) {
+	$.fn.mouseHover = function(options, enterCallback, leaveCallback) {
+		var defaults = {
+			className : "hover",
+		}
+		var options = $.extend(defaults, options);
+		this.each(function() {
+			var that = $(this);
+			that.hover(function() {// 鼠标经过
+				that.addClass(options.className);
+				if (enterCallback) {
+					enterCallback();
+				}
+			}, function() {// 鼠标离开
+				that.removeClass(options.className);
+				if (leaveCallback) {
+					leaveCallback();
+				}
+			});
+		});
+	};
+}(window.jQuery);
+
+
+/**
+ * ==================================================================
+ * 
+ * 失去焦点事件
+ * 
+ * @param options
+ *            鼠标经过离开时样式
+ * @param focusCallback
+ *            获得焦点事件
+ * @param blurCallback
+ *            失去焦点事件
+ * ==================================================================
+ */
+
+!function($) {
+	$.fn.focusblur = function(options,focusCallback,blurCallback) {
+		var defaults = {
+			className : "focus",
+		}
+		var options = $.extend(defaults, options);
+		this.each(function() {
+			var that = $(this);
+			that.focus(function() {// 获得焦点事件
+				that.addClass(options.className).removeClass("inputError");
+				if (focusCallback) {
+					focusCallback();
+				}
+			});
+			that.blur(function() {// 失去焦点事件
+				that.removeClass(options.className);
+				if (blurCallback) {
+					blurCallback();
+				}
+			});
+		});
+	}
+}(window.jQuery);
+
+
+/**
+ * ==================================================================
+ * 
+ * 字数限制
+ * 
+ * ==================================================================
+ */
+!function($) {
+	$.fn.textarealength = function(options,minlength,maxlength){
+		var defaults = {
+			minlength:0,
+			maxlength:140,
+			errorClass:"error",
+			exceed:true,			
+		}
+		var options = $.extend(defaults, options);
+		this.each(function(){			
+			var that = $(this);
+			var v = that.val();
+			var l = v.length;
+			
+			that.on("keyup",function(){
+				v = that.val();
+				l = v.length;				
+				if (l > options.maxlength) {
+					if(options.exceed){
+						that.addClass(options.errorClass);
+					}else{
+						v = v.substring(0, options.maxlength);
+						that.val(v);
+						that.removeClass(options.errorClass);
+					}					
+				}
+				else if(l<options.minlength){
+					that.addClass(options.errorClass);
+				}else{
+					that.removeClass(options.errorClass);
+				}
+				// that.parent().find(".textarea-length").text(v.length);
+			});		
+			
+		});
+	}
+} (window.jQuery);
